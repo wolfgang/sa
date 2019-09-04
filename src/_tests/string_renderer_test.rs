14ac3cb45a71fae5e29e019abd6bd7d4dcd319f8
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-09-04 15:13:39 +0200

    Sketch out how to draw and assert "sprites"

--- src/_tests/string_renderer_test.rs
+++ src/_tests/string_renderer_test.rs
@@ -1,26 +1,42 @@
-use std::borrow::Borrow;
-
-use raylib::{Rectangle, Vector2};
+use std::char;
+use std::collections::HashMap;
 
 trait GameRenderer {
-    fn draw_sprite(position: Vector2, rectangle: Rectangle, id: u8);
+    fn draw_sprite(&mut self, id: u8, x: u32, y: u32);
 }
 
 type Frame = Vec<Vec<char>>;
 
 struct StringRenderer {
     frame: Frame,
-    width: usize,
-    height: usize,
+    sprites: HashMap<u8, (u8, u8)>
+}
+
+impl GameRenderer for StringRenderer {
+    fn draw_sprite(&mut self, id: u8, x: u32, y: u32) {
+        let (width, height) = self.sprites.get(&id).unwrap();
+        let id_char = char::from_digit(id as u32, 10).unwrap();
+
+        for row in 0..*height {
+            for column in 0..*width {
+                let pixel_x = (column as u32 + x) as usize;
+                let pixel_y = (row as u32 + y) as usize;
+                self.frame[pixel_y][pixel_x] = id_char;
+            }
+        }
+    }
 }
 
 impl StringRenderer {
     pub fn new(width: usize, height: usize) -> Self {
         StringRenderer {
             frame: vec![vec!['.'; width]; height],
-            width,
-            height,
+            sprites: HashMap::with_capacity(10)
         }
+    }
+
+    pub fn with_sprite(&mut self, id: u8, width: u8, height: u8) {
+        self.sprites.insert(id, (width, height));
     }
 
     fn assert_frame(&self, expected_frame: Vec<&str>) {
@@ -41,7 +57,7 @@ impl StringRenderer {
 
 
 #[test]
-fn assert_frame() {
+fn frame_is_empty_after_construction() {
     let sr = StringRenderer::new(3, 4);
 
     sr.assert_frame(vec![
@@ -49,5 +65,21 @@ fn assert_frame() {
         "...",
         "...",
         "..."
+    ])
+}
+
+#[test]
+fn draw_sprite_fills_rect_with_sprite_id() {
+    let mut sr = StringRenderer::new(6, 6);
+    sr.with_sprite(7, 2, 3);
+    sr.draw_sprite(7, 1, 2);
+
+    sr.assert_frame(vec![
+        "......",
+        "......",
+        ".77...",
+        ".77...",
+        ".77...",
+        "......"
     ])
 }
