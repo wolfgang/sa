7c1581388aab6b70108125e7e691fa610526a914
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-09-05 09:20:41 +0200

    Use a proper sprite sheet

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,25 @@ use raylib::{Color, Rectangle, Vector2};
 
 fn main() {
     let rl = raylib::init()
-        .size(800, 600)
+        .size(1024, 700)
         .title("Staggered Assault")
         .build();
 
-    let ship_texture = rl.load_texture("resources/ship.png");
+    let sprite_sheet = rl.load_texture("resources/spaceshooter_sheet.png");
 
     let source_rec = Rectangle {
-        x: 0.0,
+        x: 325.0,
         y: 0.0,
-        width: 32.0,
-        height: 42.0,
+        width: 98.0,
+        height: 75.0,
     };
 
-    let dest_rec = Rectangle {
-        x: 0.0,
-        y: 600.0 - 84.0,
-        width: 32.0,
-        height: 42.0,
-    };
 
     while !rl.window_should_close() {
         rl.begin_drawing();
         rl.clear_background(Color::BLACK);
 
-        rl.draw_texture_pro(&ship_texture, source_rec, dest_rec, Vector2::zero(), 0.0, Color::WHITE);
+        rl.draw_texture_rec(&sprite_sheet, source_rec, Vector2 { x: 100.0, y: 700.0 - 100.0 }, Color::WHITE);
 
         rl.end_drawing();
     }
