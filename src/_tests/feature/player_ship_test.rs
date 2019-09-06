14765914977b740785eb96d7a5cb9d741a7b48ab
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-06 13:29:27 +0200

    Verify that ship is not moving if no keys are pressed

--- src/_tests/feature/player_ship_test.rs
+++ src/_tests/feature/player_ship_test.rs
@@ -8,7 +8,6 @@ fn render_player_ship_at_bottom_of_screen_centered() {
         .with_dimensions(10, 5)
         .build();
 
-
     game.render();
     game.assert_frame(vec![
         "..........",
@@ -23,14 +22,13 @@ fn render_player_ship_at_bottom_of_screen_centered() {
 #[test]
 fn ship_movement() {
     let mut game = TestableGame::init()
-        .with_dimensions(10, 3)
+        .with_dimensions(10, 2)
         .with_fps(60)
         .with_ship_speed(120)
         .build();
 
     game.render();
     game.assert_frame(vec![
-        "..........",
         "..........",
         "...0000...",
     ]);
@@ -40,7 +38,6 @@ fn ship_movement() {
 
     game.render();
     game.assert_frame(vec![
-        "..........",
         "..........",
         ".0000.....",
     ]);
@@ -54,6 +51,15 @@ fn ship_movement() {
     game.render();
     game.assert_frame(vec![
         "..........",
+        ".....0000.",
+    ]);
+
+    game.key_is_up(KEY_RIGHT);
+    game.tick();
+    game.tick();
+
+    game.render();
+    game.assert_frame(vec![
         "..........",
         ".....0000.",
     ]);
