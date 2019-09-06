2fa6725e2bf3440ca6344df44a6c3c22ccecc62e
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-06 15:01:19 +0200

    Use tuple instead of raylib vector

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,5 +1,4 @@
 use raylib::consts::*;
-use raylib::Vector2;
 
 use builder::GameBuilder;
 use input::InputRef;
@@ -12,7 +11,7 @@ pub mod renderer;
 pub struct Game {
     dimensions: (u32, u32),
     ship_dimensions: (u32, u32),
-    ship_position: Vector2,
+    ship_position: (i32, i32),
     input: InputRef,
     fps: u32,
     ship_speed: u32,
@@ -31,27 +30,27 @@ impl Game {
             fps: builder.fps,
             dimensions: builder.dimensions,
             ship_dimensions: builder.ship_dimensions,
-            ship_position: Vector2 { x: ship_x as f32, y: ship_y as f32 },
+            ship_position: (ship_x as i32, ship_y as i32),
             ship_speed: builder.ship_speed,
         }
     }
 
     pub fn tick(&mut self) {
         if self.input.borrow().is_key_down(KEY_LEFT) {
-            self.ship_position.x -= self.ship_speed as f32 / self.fps as f32;
-            if self.ship_position.x < 0.0 { self.ship_position.x = 0.0 }
+            self.ship_position.0 -= (self.ship_speed / self.fps) as i32;
+            if self.ship_position.0 < 0 { self.ship_position.0 = 0 }
         }
 
         if self.input.borrow().is_key_down(KEY_RIGHT) {
-            self.ship_position.x += self.ship_speed as f32 / self.fps as f32;
-            if self.ship_position.x >= (self.dimensions.0 - self.ship_dimensions.0) as f32 {
-                self.ship_position.x = (self.dimensions.0 - self.ship_dimensions.0) as f32
+            self.ship_position.0 += (self.ship_speed / self.fps) as i32;
+            if self.ship_position.0 >= (self.dimensions.0 - self.ship_dimensions.0) as i32 {
+                self.ship_position.0 = (self.dimensions.0 - self.ship_dimensions.0) as i32
             }
         }
     }
 
     pub fn render(&self, renderer: &mut dyn GameRenderer) {
         renderer.clear();
-        renderer.draw_sprite(0, self.ship_position.x as i32, self.ship_position.y as i32);
+        renderer.draw_sprite(0, self.ship_position.0, self.ship_position.1);
     }
 }
