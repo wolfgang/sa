f2a308b8d4468ad09804ef49eaec9010a2f7170e
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-07 15:01:56 +0200

    Use BulletsManager and remove bullet spawning code from Game

--- src/game/bullets_manager.rs
+++ src/game/bullets_manager.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
 use crate::game::player_bullet::PlayerBullet;
@@ -5,7 +6,7 @@ use crate::game::player_ship::PlayerShip;
 use crate::game::renderer::GameRenderer;
 
 pub struct BulletsManager {
-    player_ship: Rc<PlayerShip>,
+    player_ship: Rc<RefCell<PlayerShip>>,
     bullets: Vec<PlayerBullet>,
     bullet_dimensions: (u32, u32),
     last_bullet_tick: u32,
@@ -14,7 +15,7 @@ pub struct BulletsManager {
 }
 
 impl BulletsManager {
-    pub fn new(player_ship: Rc<PlayerShip>, fps: u32, bullet_dimensions: (u32, u32)) -> Self {
+    pub fn new(player_ship: Rc<RefCell<PlayerShip>>, fps: u32, bullet_dimensions: (u32, u32)) -> Self {
         BulletsManager {
             player_ship: player_ship.clone(),
             bullets: Vec::with_capacity(10),
@@ -35,13 +36,12 @@ impl BulletsManager {
         if seconds_since_last >= 0.5 {
             self.last_bullet_tick = self.current_tick;
             let (bullet_width, bullet_height) = self.bullet_dimensions;
-            let (x, y) = self.player_ship.bullet_spawn_position(bullet_width as i32, bullet_height as i32);
+            let (x, y) = self.player_ship.borrow().bullet_spawn_position(bullet_width as i32, bullet_height as i32);
             self.bullets.push(PlayerBullet::new(x, y))
         }
     }
 
     pub fn render_bullets(&self, renderer: &mut dyn GameRenderer) {
-        renderer.clear();
         for bullet in &self.bullets {
             bullet.render(renderer);
         }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,8 +1,10 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use raylib::consts::*;
 
 use builder::GameBuilder;
 use input::InputRef;
-use player_bullet::PlayerBullet;
 use player_ship::PlayerShip;
 use renderer::GameRenderer;
 
@@ -20,11 +22,8 @@ pub struct Game {
     input: InputRef,
     fps: u32,
     ship_speed: u32,
-    player_ship: PlayerShip,
-    player_bullets: Vec<PlayerBullet>,
-    bullet_dimensions: (u32, u32),
-    last_bullet_tick: u32,
-    current_tick: u32
+    player_ship: Rc<RefCell<PlayerShip>>,
+    bullets_manager: BulletsManager
 }
 
 impl Game {
@@ -33,48 +32,36 @@ impl Game {
     }
 
     pub fn from_builder(builder: &GameBuilder) -> Self {
+        let player_ship = Rc::new(RefCell::new(PlayerShip::new(builder.ship_dimensions, builder.dimensions)));
         Game {
             input: builder.input.clone(),
             fps: builder.fps,
-            player_ship: PlayerShip::new(builder.ship_dimensions, builder.dimensions),
+            player_ship: player_ship.clone(),
             ship_speed: builder.ship_speed,
-            player_bullets: Vec::with_capacity(10),
-            bullet_dimensions: builder.bullet_dimensions,
-            last_bullet_tick: 0,
-            current_tick: 1000,
+            bullets_manager: BulletsManager::new(player_ship.clone(), builder.fps, builder.bullet_dimensions)
         }
     }
 
     pub fn tick(&mut self) {
         let offset = (self.ship_speed / self.fps) as i32;
         if self.input.borrow().is_key_down(KEY_LEFT) {
-            self.player_ship.move_horizontally(-1 * offset);
+            self.player_ship.borrow_mut().move_horizontally(-1 * offset);
         }
 
         if self.input.borrow().is_key_down(KEY_RIGHT) {
-            self.player_ship.move_horizontally(offset);
+            self.player_ship.borrow_mut().move_horizontally(offset);
         }
 
         if self.input.borrow().is_key_down(KEY_SPACE) {
-            let seconds_since_last = (self.current_tick - self.last_bullet_tick) as f32 / self.fps as f32;
-
-            if seconds_since_last >= 0.5 {
-                self.last_bullet_tick = self.current_tick;
-                let (bullet_width, bullet_height) = self.bullet_dimensions;
-                let (x, y) = self.player_ship.bullet_spawn_position(bullet_width as i32, bullet_height as i32);
-                self.player_bullets.push(PlayerBullet::new(x, y))
-            }
+            self.bullets_manager.spawn_bullet();
         }
+        self.bullets_manager.tick();
 
-        self.current_tick += 1;
     }
 
     pub fn render(&self, renderer: &mut dyn GameRenderer) {
         renderer.clear();
-        self.player_ship.render(renderer);
-
-        for bullet in &self.player_bullets {
-            bullet.render(renderer);
-        }
+        self.player_ship.borrow_mut().render(renderer);
+        self.bullets_manager.render_bullets(renderer);
     }
 }
