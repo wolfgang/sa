18e4598ae901574e1594d5457a92710ca944f8a0
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-07 15:38:26 +0200

    Pass fps to bullet

--- src/game/player_bullet.rs
+++ src/game/player_bullet.rs
@@ -6,15 +6,16 @@ const BULLET_SPEED: u32 = 60;
 pub struct PlayerBullet {
     x: i32,
     y: i32,
+    fps: f32
 }
 
 impl PlayerBullet {
-    pub fn new(x: i32, y: i32) -> Self {
-        Self { x, y }
+    pub fn new(x: i32, y: i32, fps: u32) -> Self {
+        Self { x, y, fps: fps as f32 }
     }
 
     pub fn tick(&mut self) {
-        self.y = (self.y as f32 - BULLET_SPEED as f32 / 60.0) as i32
+        self.y = (self.y as f32 - BULLET_SPEED as f32 / self.fps) as i32
     }
 
     pub fn render(&self, renderer: &mut dyn GameRenderer) {
