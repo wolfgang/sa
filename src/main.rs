fc719be045a04efa7751953fb0ed3f183484adb3
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-08 14:32:48 +0200

    Use SpriteRegistry to load ship and bullet sprites

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
+use std::fs;
 use std::rc::Rc;
-
-use raylib::Rectangle;
 
 use sa::game::Game;
 use sa::raylib::{input::RaylibInput, renderer::RaylibRenderer};
+use sa::raylib::sprite_registry::SpriteRegistry;
 
 fn main() {
     let screen_width = 650;
     let screen_height = 800;
-    let ship_width = 98;
-    let ship_height = 75;
-    let bullet_width = 48;
-    let bullet_height = 46;
     let fps = 60;
 
     let rl = Rc::new(
@@ -20,38 +16,27 @@ fn main() {
             .title("Staggered Assault")
             .build());
 
+    let sprite_xml = fs::read_to_string("resources/spaceshooter_sheet.xml").unwrap();
+    let sprite_registry = SpriteRegistry::from_xml(sprite_xml);
+
+    let ship_rec = sprite_registry.get_source_rec("playerShip3_red.png");
+    let bullet_rec = sprite_registry.get_source_rec("laserBlue08.png");
 
     let mut game = Game::init()
         .with_input(RaylibInput::new_rc(rl.clone()))
         .with_dimensions(screen_width as u32, screen_height as u32)
-        .with_ship_dimensions(ship_width, ship_height)
-        .with_bullet_dimensions(bullet_width, bullet_height)
+        .with_ship_dimensions(ship_rec.width as u32, ship_rec.height as u32)
+        .with_bullet_dimensions(bullet_rec.width as u32, bullet_rec.height as u32)
         .with_fps(fps)
         .with_ship_speed(360)
         .with_bullet_speed(540)
         .build();
 
     let mut renderer = RaylibRenderer::new(rl.clone());
-
-    let ship_rec = Rectangle {
-        x: 325.0,
-        y: 0.0,
-        width: ship_width as f32,
-        height: ship_height as f32,
-    };
-
-    let bullet_rec = Rectangle {
-        x: 596.0,
-        y: 961.0,
-        width: bullet_width as f32,
-        height: bullet_height as f32,
-    };
-
     renderer.register_sprite(0, ship_rec);
     renderer.register_sprite(1, bullet_rec);
 
     let frame_time = 1.0 / fps as f64;
-
     let mut current_delta = 0.0;
     let mut last_time = rl.get_time();
 
