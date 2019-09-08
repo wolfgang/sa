de0d826175cdf61495504d600e4894d623d682af
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-08 14:21:08 +0200

    Parse SpriteRegistry from xml

--- src/_tests/unit/sprite_registry_test.rs
+++ src/_tests/unit/sprite_registry_test.rs
@@ -1,11 +1,5 @@
-use std::collections::HashMap;
-
-use raylib::Rectangle;
-use xmlparser as xml;
-
 use crate::raylib::sprite_registry::SpriteRegistry;
 
-#[ignore]
 #[test]
 fn get_source_rec_by_name() {
     let xml = r#"<TextureAtlas imagePath="sheet.png">
@@ -19,71 +13,16 @@ fn get_source_rec_by_name() {
 
     let registry = SpriteRegistry::from_xml(xml.to_string());
 
-    let rec = registry.get_source_rec("beam02.png");
+    let beam2 = registry.get_source_rec("beam2.png");
+    assert_eq!(beam2.x, 262.0);
+    assert_eq!(beam2.y, 907.0);
+    assert_eq!(beam2.width, 38.0);
+    assert_eq!(beam2.height, 31.0);
 
-    assert_eq!(rec.x, 262.0);
-    assert_eq!(rec.y, 907.0);
-    assert_eq!(rec.width, 38.0);
-    assert_eq!(rec.height, 31.0)
-}
+    let beam4 = registry.get_source_rec("beam4.png");
+    assert_eq!(beam4.x, 177.0);
+    assert_eq!(beam4.y, 496.0);
+    assert_eq!(beam4.width, 41.0);
+    assert_eq!(beam4.height, 17.0);
 
-#[test]
-fn xml_parsing() {
-    let xml = r#"<TextureAtlas imagePath="sheet.png">
-                    <SubTexture name="beam0.png" x="143" y="377" width="43" height="31"/>
-                    <SubTexture name="beam1.png" x="327" y="644" width="40" height="20"/>
-                </TextureAtlas>
-                "#;
-
-
-    let mut current_rec_opt: Option<(String, Rectangle)> = Option::None;
-
-
-    let mut recs: HashMap<String, Rectangle> = HashMap::with_capacity(100);
-
-    let mut current_name = String::from("");
-    let mut current_rec = Rectangle { x: 0.0, y: 0.0, width: 0.0, height: 0.0 };
-
-
-    for token in xmlparser::Tokenizer::from(xml) {
-        match token {
-            Ok(xml::Token::Attribute { prefix, local, value, span }) => {
-                match local.as_str() {
-                    "name" => {
-                        if current_name.to_string() != String::from("") {
-                            recs.insert(current_name.to_string(), current_rec);
-                        }
-                        current_name = value.as_str().to_string();
-                        current_rec = Rectangle { x: 0.0, y: 0.0, width: 0.0, height: 0.0 };
-                    }
-                    "x" => { current_rec.x = value.as_str().parse().unwrap() }
-                    "y" => { current_rec.y = value.as_str().parse().unwrap() }
-                    "width" => { current_rec.width = value.as_str().parse().unwrap() }
-                    "height" => { current_rec.height = value.as_str().parse().unwrap() }
-
-                    _ => {}
-                }
-            }
-            _ => {}
-        }
-    }
-
-    recs.insert(current_name.to_string(), current_rec);
-
-    assert_eq!(recs.len(), 2);
-
-    let beam0 = recs.get("beam0.png").unwrap();
-    assert_eq!(beam0.x, 143.0);
-    assert_eq!(beam0.y, 377.0);
-    assert_eq!(beam0.width, 43.0);
-    assert_eq!(beam0.height, 31.0);
-
-    let beam1 = recs.get("beam1.png").unwrap();
-    assert_eq!(beam1.x, 327.0);
-    assert_eq!(beam1.y, 644.0);
-    assert_eq!(beam1.width, 40.0);
-    assert_eq!(beam1.height, 20.0);
-
-
-//    assert_eq!(1, 2)
 }
--- src/raylib/sprite_registry.rs
+++ src/raylib/sprite_registry.rs
@@ -1,18 +1,61 @@
-use raylib::Rectangle;
+use std::collections::HashMap;
 
-pub struct SpriteRegistry {}
+use raylib::Rectangle;
+use xmlparser as xml;
+use xmlparser::StrSpan;
+
+pub struct SpriteRegistry {
+    recs: HashMap<String, Rectangle>
+}
 
 impl SpriteRegistry {
-    pub fn from_xml(xml: String) -> Self {
-        Self {}
+    pub fn from_xml(source_xml: String) -> Self {
+        Self { recs: Self::parse(source_xml) }
+    }
+
+    fn parse(source_xml: String) -> HashMap<String, Rectangle> {
+        let mut recs: HashMap<String, Rectangle> = HashMap::with_capacity(100);
+        let mut current_name = String::from("");
+        let mut current_rec = Self::new_rec();
+
+
+        for token in xmlparser::Tokenizer::from(source_xml.as_str()) {
+            match token {
+                Ok(xml::Token::Attribute { prefix: _, local, value, span: _ }) => {
+                    match local.as_str() {
+                        "name" => {
+                            if !current_name.is_empty() {
+                                recs.insert(current_name.to_string(), current_rec);
+                            }
+                            current_name = value.to_string();
+                            current_rec = Self::new_rec();
+                        }
+                        "x" => { current_rec.x = Self::value_to_float(value) }
+                        "y" => { current_rec.y = Self::value_to_float(value) }
+                        "width" => { current_rec.width = Self::value_to_float(value) }
+                        "height" => { current_rec.height = Self::value_to_float(value) }
+
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        recs.insert(current_name.to_string(), current_rec);
+        recs
+
     }
 
     pub fn get_source_rec(&self, name: &str) -> Rectangle {
-        Rectangle {
-            x: 0.0,
-            y: 0.0,
-            width: 0.0,
-            height: 0.0
-        }
+        self.recs.get(name).unwrap().clone()
+    }
+
+    fn value_to_float(value: StrSpan) -> f32 {
+        value.as_str().parse().unwrap()
+    }
+
+    fn new_rec() -> Rectangle {
+        Rectangle { x: 0.0, y: 0.0, width: 0.0, height: 0.0 }
     }
 }
