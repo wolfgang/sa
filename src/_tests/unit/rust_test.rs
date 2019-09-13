078902239159d9567f2b9a70cf8e9e4b19cd9797
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-13 19:56:50 +0200

    Verify that trait objects can be stored in Rc<RefCell> objects

--- src/_tests/unit/rust_test.rs
+++ src/_tests/unit/rust_test.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 struct Test1 {
     x: u32
 }
@@ -14,8 +17,35 @@ impl Test1 {
 }
 
 #[test]
-fn nothing() {
+fn call_mut_function_from_mut_function() {
     let mut obj = Test1 { x: 11 };
     obj.inc();
     assert_eq!(obj.x, 14);
+}
+
+trait TestTrait {
+    fn add_this(&self, v: u32) -> u32;
+}
+
+
+struct TestTraitImpl {
+    base: u32
+}
+
+impl TestTrait for TestTraitImpl {
+    fn add_this(&self, v: u32) -> u32 {
+        self.base + v
+    }
+}
+
+type TraitRef = Rc<RefCell<dyn TestTrait>>;
+
+#[test]
+fn vec_of_trait_objects() {
+    let mut v: Vec<TraitRef> = Vec::new();
+
+    let obj = Rc::new(RefCell::new(TestTraitImpl { base: 100 }));
+    v.push(obj.clone());
+
+    assert_eq!(v[0].borrow().add_this(20), 120);
 }
