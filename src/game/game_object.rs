cb140c89f1227c46eb95b4c916b744b9fd36fd84
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-13 09:30:18 +0200

    Change GameObject move_direction to Vector2

--- src/game/game_object.rs
+++ src/game/game_object.rs
@@ -6,7 +6,7 @@ pub struct GameObject {
     sprite_id: u8,
     position: Vector2<i32>,
     speed: Vector2<u32>,
-    move_direction: (i32, i32),
+    move_direction: Vector2<i32>,
 
 }
 
@@ -16,13 +16,13 @@ impl GameObject {
             sprite_id,
             position: Vector2::new(x, y),
             speed: Vector2::from(speed),
-            move_direction: (0, 0),
+            move_direction: Vector2::new(0, 0)
         }
     }
 
     pub fn tick(&mut self) {
-        self.position.x += self.move_direction.0 * self.speed.x as i32;
-        self.position.y += self.move_direction.1 * self.speed.y as i32;
+        self.position.x += self.move_direction.x * self.speed.x as i32;
+        self.position.y += self.move_direction.y * self.speed.y as i32;
     }
 
 
@@ -36,7 +36,8 @@ impl GameObject {
     }
 
     pub fn set_move_direction(&mut self, dx: i32, dy: i32) {
-        self.move_direction = (dx, dy);
+        self.move_direction.x = dx;
+        self.move_direction.y = dy;
     }
 
     pub fn get_position(&self) -> (i32, i32) {
