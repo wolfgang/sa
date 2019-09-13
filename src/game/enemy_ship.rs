4acdc9a78e80732aac92ec8f2e17b1e96d29dbf6
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-13 14:35:15 +0200

    Simplify

--- src/game/enemy_ship.rs
+++ src/game/enemy_ship.rs
@@ -5,7 +5,7 @@ use crate::game::renderer::GameRenderer;
 pub struct EnemyShip {
     width: u32,
     max_x: u32,
-    game_object: GameObject
+    game_object: GameObject,
 }
 
 impl EnemyShip {
@@ -15,7 +15,7 @@ impl EnemyShip {
         Self {
             width: builder.enemy_dimensions.0,
             max_x: builder.dimensions.0,
-            game_object
+            game_object,
         }
     }
 
@@ -23,17 +23,11 @@ impl EnemyShip {
         self.game_object.tick();
 
         let pos = self.game_object.get_position();
-
-        if pos.x < 0.0 {
-            self.game_object.set_position(0.0, pos.y);
-            self.game_object.set_move_direction(1, 1);
-        }
-
         let max_x = (self.max_x - self.width) as f32;
-        if pos.x > max_x {
-            self.game_object.set_position(max_x, pos.y);
-            self.game_object.set_move_direction(-1, 1);
-        }
+
+        if pos.x < 0.0 { self.game_object.set_move_direction(1, 1) }
+        if pos.x > max_x { self.game_object.set_move_direction(-1, 1) }
+        self.game_object.set_position(f32::max(0.0, f32::min(pos.x, max_x)), pos.y);
     }
 
     pub fn render<T>(&self, renderer: &mut T) where T: GameRenderer {
