dcac9b01d8dcb3694b68813649c05a14dfae04c4
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-16 16:24:35 +0200

    Playing games with rust generics

--- src/_tests/unit/generic_events_test.rs
+++ src/_tests/unit/generic_events_test.rs
@@ -1,4 +1,12 @@
-trait Event {}
+use std::cell::RefCell;
+use std::rc::Rc;
+
+const EVENT1: u32 = 1;
+const EVENT2: u32 = 2;
+
+trait Event {
+    fn id(&self) -> u32;
+}
 
 trait EventHandler<T> where T: Event {
     fn handle(&mut self, event: T);
@@ -8,9 +16,18 @@ struct Event1 { int_val: i32 }
 
 struct Event2 { float_val: f32 }
 
-impl Event for Event1 {}
+impl Event for Event1 {
+    fn id(&self) -> u32 {
+        EVENT1
+    }
+}
 
-impl Event for Event2 {}
+impl Event for Event2 {
+    fn id(&self) -> u32 {
+        EVENT2
+    }
+}
+
 
 #[derive(Default)]
 struct TestEventHandler {
@@ -30,6 +47,30 @@ impl EventHandler<Event2> for TestEventHandler {
     }
 }
 
+type EventHandlerRef<T> = Rc<RefCell<dyn EventHandler<T>>>;
+
+trait EventDispatcher<T> where T: Event {
+    fn on(&mut self, handler: EventHandlerRef<T>);
+}
+
+#[derive(Default)]
+struct TestEventDispatcher {
+    handlers_event1: Vec<EventHandlerRef<Event1>>,
+    handlers_event2: Vec<EventHandlerRef<Event2>>,
+}
+
+impl EventDispatcher<Event1> for TestEventDispatcher {
+    fn on(&mut self, handler: EventHandlerRef<Event1>) {
+        self.handlers_event1.push(handler.clone());
+    }
+}
+
+impl EventDispatcher<Event2> for TestEventDispatcher {
+    fn on(&mut self, handler: EventHandlerRef<Event2>) {
+        self.handlers_event2.push(handler.clone());
+    }
+}
+
 
 #[test]
 fn nothing() {
@@ -38,4 +79,15 @@ fn nothing() {
     assert_eq!(handler.int_val_received, 123);
     handler.handle(Event2 { float_val: 456.789 });
     assert_eq!(handler.float_val_received, 456.789);
+}
+
+#[test]
+fn dispatching_of_events() {
+    let mut dispatcher = TestEventDispatcher::default();
+    let handler = Rc::new(RefCell::new(TestEventHandler::default()));
+    dispatcher.on(handler.clone() as Rc<RefCell<dyn EventHandler<Event1>>>);
+    dispatcher.on(handler.clone() as Rc<RefCell<dyn EventHandler<Event2>>>);
+
+    assert_eq!(1, dispatcher.handlers_event1.len());
+    assert_eq!(1, dispatcher.handlers_event2.len());
 }
