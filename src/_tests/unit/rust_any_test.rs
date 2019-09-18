5d737d2a951ace8884e01e4658d90dd0b338e449
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-18 11:49:28 +0200

    Store any types in a map

--- src/_tests/unit/rust_any_test.rs
+++ src/_tests/unit/rust_any_test.rs
@@ -1,6 +1,28 @@
 use std::any::Any;
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::rc::Rc;
+
+type AnyRef = Box<dyn Any>;
+
+struct AnyMap {
+    values: HashMap<u32, AnyRef>
+}
+
+impl AnyMap {
+    fn new() -> Self {
+        Self { values: HashMap::with_capacity(100) }
+    }
+
+    fn add<T>(&mut self, key: u32, value: T) where T: Any {
+        self.values.insert(key, Box::from(value));
+    }
+
+    fn get_ref<T>(&self, key: u32) -> &T where T: Any {
+        let v = self.values.get(&key).unwrap();
+        (*v).downcast_ref::<T>().unwrap()
+    }
+}
 
 #[test]
 fn any_in_rc() {
@@ -19,4 +41,15 @@ fn any_in_refcell() {
     s.push('x');
 
     assert_eq!(s.as_str(), "Hellox")
+}
+
+#[test]
+fn store_any_types() {
+    let mut map = AnyMap::new();
+
+    map.add(1, String::from("hello"));
+    map.add(2, 1234 as u32);
+
+    assert_eq!(map.get_ref::<String>(1).as_str(), "hello");
+    assert_eq!(map.get_ref::<u32>(2), &1234)
 }
