e4db6e0234dccfd317fea38df7bab762a1965a51
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-18 15:49:40 +0200

    Prototype a map with a custom any-convertible trait

--- src/_tests/unit/rust_any_test.rs
+++ src/_tests/unit/rust_any_test.rs
@@ -47,6 +47,64 @@ impl AnyMap {
     }
 }
 
+
+type ComponentRef = Box<dyn Component>;
+
+struct ComponentMap {
+    values: HashMap<TypeId, HashMap<u32, ComponentRef>>,
+}
+
+impl ComponentMap {
+    fn new() -> Self {
+        Self {
+            values: HashMap::default(),
+        }
+    }
+
+    fn insert<T>(&mut self, key: u32, value: T) where T: Component + 'static {
+        let type_id = TypeId::of::<T>();
+        let map_for_type = self.values.entry(type_id).or_insert(HashMap::with_capacity(10));
+        map_for_type.insert(key, Box::from(value));
+    }
+
+    fn get_ref<T>(&self, key: u32) -> &T where T: Component + 'static {
+        let type_id = TypeId::of::<T>();
+        let map_for_type = self.values.get(&type_id).unwrap();
+        let v = map_for_type.get(&key).unwrap();
+        Self::downcast_boxed(v)
+    }
+
+    fn get_mut_ref<T>(&mut self, key: u32) -> &mut T where T: Component + 'static {
+        let type_id = TypeId::of::<T>();
+        let map_for_type = self.values.get_mut(&type_id).unwrap();
+        let v = map_for_type.get_mut(&key).unwrap();
+        (*v).as_any_mut().downcast_mut::<T>().unwrap()
+    }
+
+    fn get_all<T>(&self) -> Vec<&T> where T: Component + 'static {
+        let type_id = TypeId::of::<T>();
+        let map_for_type = self.values.get(&type_id).unwrap();
+        map_for_type.values().map(|v| Self::downcast_boxed(v)).collect()
+    }
+
+    fn get_all_components(&self) -> Vec<&Box<dyn Component>> {
+        let mut result = Vec::new();
+
+        for v in self.values.values() {
+            for b in v.values() {
+                result.push(b);
+            }
+        }
+
+        result
+    }
+
+    fn downcast_boxed<T>(boxed: &ComponentRef) -> &T where T: Component + 'static {
+        (*boxed).as_any().downcast_ref::<T>().unwrap()
+    }
+}
+
+
 #[test]
 fn any_in_rc() {
     let v: Rc<dyn Any> = Rc::new(String::from("Hello"));
@@ -132,4 +190,53 @@ fn get_all_values_for_type() {
     assert_eq!(values.len(), 2);
     assert!(values.contains(&&String::from("one")));
     assert!(values.contains(&&String::from("two")));
+}
+
+trait AsAny: Any {
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+
+trait Component: AsAny {}
+
+#[derive(PartialEq, Debug)]
+struct SomeComponent {
+    x: i32
+}
+
+impl Component for SomeComponent {}
+
+#[derive(PartialEq, Debug)]
+struct SomeOtherComponent {
+    y: u32
+}
+
+impl Component for SomeOtherComponent {}
+
+
+impl<T: Component + 'static> AsAny for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+#[test]
+fn component_map() {
+    let mut map = ComponentMap::new();
+
+    map.insert(1, SomeComponent { x: 1234 });
+    map.insert(1, SomeOtherComponent { y: 1234 });
+    map.insert(2, SomeComponent { x: 5678 });
+    assert_eq!(map.get_ref::<SomeComponent>(1), &SomeComponent { x: 1234 });
+    assert_eq!(map.get_ref::<SomeOtherComponent>(1), &SomeOtherComponent { y: 1234 });
+    assert_eq!(map.get_ref::<SomeComponent>(2), &SomeComponent { x: 5678 });
+
+    let all = map.get_all_components();
+    assert_eq!(all.len(), 3);
+    let c = all[0];
 }
