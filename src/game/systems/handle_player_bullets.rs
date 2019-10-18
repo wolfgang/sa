ad4a89acc068560f30a26f62b0420006b1e45216
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-10-18 19:45:25 +0200

    Clean up

--- src/game/systems/handle_player_bullets.rs
+++ src/game/systems/handle_player_bullets.rs
@@ -30,20 +30,26 @@ impl<'a> System<'a> for HandlePlayerBullets {
                 if game_state.current_tick - game_state.last_bullet_tick >= config.autofire_delay {
                     game_state.last_bullet_tick = game_state.current_tick;
                     let bullet = entities.create();
-                    let x = player_geom.x as u32 + config.ship_dimensions.0 / 2 - config.bullet_dimensions.0 / 2;
-                    let y = player_geom.y as u32 - config.bullet_dimensions.1;
-                    updater.insert(bullet, Geometry {
-                        x: x as i32,
-                        y: y as i32,
-                        width: config.bullet_dimensions.0,
-                        height: config.bullet_dimensions.1,
-                    });
+                    updater.insert(bullet, Self::get_bullet_geometry(&config, player_geom));
                     updater.insert(bullet, Velocity(0, -1 * config.bullet_speed as i32));
                     updater.insert(bullet, Sprite { id: 1 });
                 }
             } else {
                 game_state.last_bullet_tick = 0;
             }
+        }
+    }
+}
+
+impl HandlePlayerBullets {
+    fn get_bullet_geometry(config: &GameConfig, player_geom: &Geometry) -> Geometry {
+        let x = player_geom.x as u32 + config.ship_dimensions.0 / 2 - config.bullet_dimensions.0 / 2;
+        let y = player_geom.y as u32 - config.bullet_dimensions.1;
+        Geometry {
+            x: x as i32,
+            y: y as i32,
+            width: config.bullet_dimensions.0,
+            height: config.bullet_dimensions.1,
         }
     }
 }
