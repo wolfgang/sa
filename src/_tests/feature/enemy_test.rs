8afd26d5adf65e22367de2c9977974ce17d74a9c
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-10-19 18:21:24 +0200

    Shrink test screen

--- src/_tests/feature/enemy_test.rs
+++ src/_tests/feature/enemy_test.rs
@@ -3,7 +3,7 @@ use crate::_tests::helpers::testable_game::TestableGame;
 #[test]
 fn enemy_moves_from_left_to_right() {
     let mut game = TestableGame::init()
-        .with_dimensions(10, 5)
+        .with_dimensions(10, 3)
         .with_enemy_count(1)
         .with_enemy_speed(4, 0)
         .build();
@@ -11,16 +11,12 @@ fn enemy_moves_from_left_to_right() {
     game.renders_frame(vec![
         "222.......",
         "..........",
-        "..........",
-        "..........",
         "...0000..."
     ]);
 
     game.tick();
     game.renders_frame(vec![
         "....222...",
-        "..........",
-        "..........",
         "..........",
         "...0000..."
     ]);
@@ -29,16 +25,12 @@ fn enemy_moves_from_left_to_right() {
     game.renders_frame(vec![
         ".......222",
         "..........",
-        "..........",
-        "..........",
         "...0000..."
     ]);
 
     game.tick();
     game.renders_frame(vec![
         "...222....",
-        "..........",
-        "..........",
         "..........",
         "...0000..."
     ]);
@@ -47,16 +39,12 @@ fn enemy_moves_from_left_to_right() {
     game.renders_frame(vec![
         "222.......",
         "..........",
-        "..........",
-        "..........",
         "...0000..."
     ]);
 
     game.tick();
     game.renders_frame(vec![
         "....222...",
-        "..........",
-        "..........",
         "..........",
         "...0000..."
     ]);
